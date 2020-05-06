c79acb9346e243666b7ff32296a084e90b2201b1
Burak Teke <tr.burakteke@gmail.com>
2020-05-06 04:24:08 +0300

    Update to np.bool

--- sample/coco.py
+++ sample/coco.py
@@ -76,7 +76,7 @@ def kp_detection(db, k_ind, data_aug, debug):
     tl_tags     = np.zeros((batch_size, max_tag_len), dtype=np.int64)
     br_tags     = np.zeros((batch_size, max_tag_len), dtype=np.int64)
     ct_tags     = np.zeros((batch_size, max_tag_len), dtype=np.int64)
-    tag_masks   = np.zeros((batch_size, max_tag_len), dtype=np.uint8)
+    tag_masks   = np.zeros((batch_size, max_tag_len), dtype=np.bool)
     tag_lens    = np.zeros((batch_size, ), dtype=np.int32)
 
     db_size = db.db_inds.size
